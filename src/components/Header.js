f33b03c095413f7fd565eb634744567dbb6bf2fe
jonhayjr <jonhayjr@gmail.com>
2022-01-22 10:33:10 -0800

    Created components folder and Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <header className="bg-primary p-3">
+          <h1 className="h1 text-white">Product Review App</h1>
+      </header>
+  )
+
+};
+
+export default Header;
